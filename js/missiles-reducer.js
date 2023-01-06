818b9c52b79edcbc157b176c996cc537deb3a117
pepsif <pepsi_fack@ukr.net>
2023-01-06 10:08:15 +0200

    add: add aim7m in missiles directory and edit missile-reducer,js

--- js/missiles-reducer.js
+++ js/missiles-reducer.js
@@ -1,5 +1,5 @@
 const missilesListTitle = document.querySelector(".missiles-list-title");
-const missilesList = document.querySelector(".missiles-list");
+const missilesList = document.querySelector(".nato-missiles-list");
 const missilesItem = document.querySelector(".missiles-item");
 const iframe = document.getElementById("main-iframe");
 
@@ -7,17 +7,22 @@ console.log(iframe.getAttribute("src"))
 missilesList.addEventListener('click', setMissile)
 
 function setMissile(event) {
-    
        if(event.target.textContent === "aim 120") {
          iframe.setAttribute("src","./aim-120.html")
        }
        if(event.target.textContent === "sidewinder") {
         iframe.setAttribute("src","./AIM-9X-Sidewinder.html")
       }
-           
-      
-
-
+      if(event.target.textContent === "aim 120b") {
+        iframe.setAttribute("src","./aim120b.html")
+      }
+      if(event.target.textContent === "aim 120c") {
+        iframe.setAttribute("src","./aim120c.html")
+      }
+      if(event.target.textContent === "AIM-7М Sparrow") {
+        iframe.setAttribute("src","./aim7msparrow.html")
+      }
+                
 }
 
 
