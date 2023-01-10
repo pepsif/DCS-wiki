7b8e1438ff0c1bfea85850fe84240a32fb4d618e
pepsi <pepsi_fack@ukr.net>
2023-01-10 11:56:03 +0200

    feat: on off enable in two missiles list in missiles-reducer.js

--- js/missiles-reducer.js
+++ js/missiles-reducer.js
@@ -1,50 +1,47 @@
+const mainList = document.querySelector(".main-list");
 const missilesListTitle = document.querySelector(".missiles-list-title");
-const missilesList = document.querySelector(".nato-missiles-list");
+const natoMissilesList = document.querySelector(".nato-missiles-list");
+const russiaMissilesList = document.querySelector(".russia-missiles-list");
 const missilesItem = document.querySelector(".missiles-item");
 const iframe = document.getElementById("main-iframe");
 
-console.log(iframe.getAttribute("src"))
-missilesList.addEventListener('click', setMissile)
+console.log(russiaMissilesList)
+mainList.addEventListener('click', onOffMissilesList)
 
-function setMissile(event) {
-       if(event.target.textContent === "aim 120") {
-         iframe.setAttribute("src","./aim-120.html")
-       }
-       if(event.target.textContent === "sidewinder") {
-        iframe.setAttribute("src","./AIM-9X-Sidewinder.html")
-      }
-      if(event.target.textContent === "aim 120b") {
-        iframe.setAttribute("src","./aim120b.html")
-      }
-      if(event.target.textContent === "aim 120c") {
-        iframe.setAttribute("src","./aim120c.html")
-      }
-      if(event.target.textContent === "AIM-7М Sparrow") {
-        iframe.setAttribute("src","./aim7msparrow.html")
-      }
-                
+function onOffMissilesList(event) {
+
+  console.log(event.target.textContent)
+
+  if (event.target.textContent === "missiles NATO") {
+    natoMissilesList.classList.toggle("active")
+    natoMissilesList.classList.toggle("")
+  }
+  if (event.target.textContent === "missiles russia") {
+    russiaMissilesList.classList.toggle("active")
+    russiaMissilesList.classList.toggle("")
+  }
+
+setMissile(event)
 }
 
 
 
-
-
-
-
-let count = 0; // missiles NATO counter on off
-missilesListTitle.onclick = () => {
-
-    let items = missilesList.children;
-    if (count === 0) {
-        for (items of missilesList.children) {
-            items.style.display = "block";
-        }
-        count += 1;
-    } else if (count === 1) {
-        for (items of missilesList.children) {
-            items.style.display = "none";
-        }
-        count = 0;
-    }
+function setMissile(event) {
+  // console.log(event.target.textContent)
+  if (event.target.textContent === "aim 120") {
+    iframe.setAttribute("src", "./aim-120.html")
+  }
+  if (event.target.textContent === "sidewinder") {
+    iframe.setAttribute("src", "./AIM-9X-Sidewinder.html")
+  }
+  if (event.target.textContent === "aim 120b") {
+    iframe.setAttribute("src", "./aim120b.html")
+  }
+  if (event.target.textContent === "aim 120c") {
+    iframe.setAttribute("src", "./aim120c.html")
+  }
+  if (event.target.textContent === "AIM-7М Sparrow") {
+    iframe.setAttribute("src", "./aim7msparrow.html")
+  }
 }
 
